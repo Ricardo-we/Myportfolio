77069cedfb4551f5fa8ff640bb7d2dd40c63a41d
Ricardo-we <totgamer75@gmail.com>
2022-02-27 21:34:16 -0600

    porfolio tottaly responsive

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -149,9 +149,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL CONFIG
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'ricardo0406dev@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_PASSWORD = 'kari0406'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'ricardo0406dev@gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST_PASSWORD = 'kari0406'
+# EMAIL_USE_TLS = True
