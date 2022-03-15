8c86b3b0118c017d4f32ede23b4bed0f9acde73d
Ricardo-we <totgamer75@gmail.com>
2022-03-14 22:41:13 -0600

    DEBUG=False

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8pdogs6#&mu9!0#_jbq*s!72$2dg2wvpi99myin=^y4ty_i!#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djangoproyectocompleto.herokuapp.com', '127.0.0.1']
 
